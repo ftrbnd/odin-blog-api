985082fa984fa711fa6ab591c3c1a654a1f75b37
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-08-04 11:34:19 -0700

    Connect to mongodb

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
+
+// Set up mongoose connection
+mongoose.set('strictQuery', false);
+async function connectDb() {
+  await mongoose.connect(process.env.MONGODB_URI);
+}
+connectDb()
+  .then(console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Failed to connect to MongoDB...', err));
 
 const app = express();
 
